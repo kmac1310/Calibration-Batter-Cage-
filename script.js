5326f91cf7d341cd74d59831cd388e1a41d7b51d
kmac1310 <mac9136@gmail.com>
2024-07-24 17:44:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const infoData = {
         text: 'A nanosecond is a billionth of a second. It\'s often used to measure extremely quick events.'
     },
     millie: {
-        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 2.gif',
+        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624%202.gif',
         text: 'A millisecond is a thousandth of a second. It\'s useful for short tasks and activities.'
     },
     second: {
-        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 80.gif',
+        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624%2080.gif',
         text: 'A second is a basic unit of time. It\'s a common unit for scheduling and time management.'
     },
     minute: {
-        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 minutes.gif',
+        image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624%20minutes.gif',
         text: 'A minute is 60 seconds long. It\'s the primary unit we use to organize our lives.'
     }
 };
