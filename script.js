fd7c2955c7e3b422bbab2560f2ab567260186432
kmac1310 <mac9136@gmail.com>
2024-07-24 10:42:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const infoData = {
   nano: {
-    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624.gif', // Use the correct URL of your GIF file
+    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624.gif',
     text: 'A nanosecond is a billionth of a second. It\'s often used to measure extremely quick events.'
   },
   millie: {
-    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 2.gif', // Use the correct URL of your GIF file
+    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 2.gif',
     text: 'A millisecond is a thousandth of a second. It\'s useful for short tasks and activities.'
   },
   second: {
-    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 80.gif', // Use the correct URL of your GIF file
+    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 80.gif',
     text: 'A second is a basic unit of time. It\'s a common unit for scheduling and time management.'
   },
   minute: {
-    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 minutes.gif', // Use the correct URL of your GIF file
+    image: 'https://kmac1310.github.io/Calibration-Batter-Cage/assets/baseball_bat_swing_10624 minutes.gif',
     text: 'A minute is 60 seconds long. It\'s the primary unit we use to organize our lives.'
   }
 };
